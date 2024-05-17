8823e90216a38949f33076bc91c4881356eaa604
Urszula Wielgosz <urszula@wielgosz.info>
2024-05-17 19:54:07 +0000

    styles update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b1571382b3891a567887');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1a96777403c48fb6868a');
